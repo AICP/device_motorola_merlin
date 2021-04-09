e2ca30908101a4bbac56f08e068999604efc92df
Tony Jose <gmoto1179@gmail.com>
2021-04-09 15:45:38 +0530

    merlin: Import Dalvik heap overrides
    * From: phone-xhdpi-2048-dalvik-heap.mk
    
    Change-Id: I79cfd35b7fa929145aa127b5d9c4d3f45aac78c2

--- system_prop.mk
+++ system_prop.mk
@@ -9,6 +9,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.boot-dex2oat-threads=8 \
     dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.mode=endfire \
